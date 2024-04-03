b0aa1680c53570c834a9fbf7cfd1df287cc8175f
MIchiel Koning <michiel.koning@valtech.com>
2024-04-03 07:59:24 +0200

    data

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,29 @@
+const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
 
   eleventyConfig.addFilter("date", (dateObj) => {
     return new Intl.DateTimeFormat("nl-NL").format(dateObj);
+  });
+
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    // Prior to Eleventy 2.0: use this.outputPath instead
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
   });
 
   return {
